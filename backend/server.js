71c1cdfb0e5f26ab7063d7841eae73b955053421
mddanish222 <mdddanish854@gmail.com>
2026-05-11 20:20:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // ── Middleware ──────────────────────────────────────────────
 app.use(cors({
-  origin: " http://localhost:5173",
+  origin: "https://voiceintel.netlify.app",
 }));
 app.use(express.json());
 
